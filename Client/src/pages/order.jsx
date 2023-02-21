d4653d5651b27d6c51b0bf363d5789108b76813c
mahesaref <mahesarefado30@gmail.com>
2023-02-21 14:36:06 +0700

    Update Order

--- Client/src/pages/order.jsx
+++ Client/src/pages/order.jsx
@@ -1,6 +1,50 @@
-import React from "react";
+import React, { useMemo } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import http from "../http";
+import axios from "axios";
 
 const Order = () => {
+  const [data, setData] = useState([]);
+  const [filterValue, setFilterValue] = useState("");
+  const [fetchStatus] = useState(true);
+
+  const getOrders = async () => {
+    try {
+      const response = await http.get(`/orders`);
+      return response.data;
+    } catch (err) {
+      return err;
+    }
+  };
+
+  const getProducts = async () => {
+    try {
+      const response = await http.get(`/products`);
+      return response.data;
+    } catch (err) {
+      return err;
+    }
+  };
+
+  const getAll = async () => {
+    const orders = await getOrders();
+    await getProducts();
+
+    setData(orders.length ? orders : []);
+  };
+
+  const handleFilterOrder = (event) => setFilterValue(event.target.value);
+
+  const filteredData = useMemo(
+    () => data.filter((item) => item.orderNo.includes(filterValue)),
+    [filterValue, data]
+  );
+
+  useEffect(() => {
+    getAll();
+  }, []);
+
   return (
     <div className="w-full bg-white">
       <div className="py-8">
@@ -10,18 +54,13 @@ const Order = () => {
             <form className="flex flex-col justify-center w-3/4 max-w-sm space-y-3 md:flex-row md:w-full md:space-x-3 md:space-y-0">
               <div className=" relative ">
                 <input
+                  onChange={handleFilterOrder}
                   type="text"
                   id='"form-subscribe-Filter'
                   className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
                   placeholder="Order No"
                 />
               </div>
-              <button
-                className="flex-shrink-0 px-4 py-2 text-base font-semibold text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200"
-                type="submit"
-              >
-                Filter
-              </button>
             </form>
           </div>
         </div>
@@ -40,140 +79,43 @@ const Order = () => {
                     scope="col"
                     className="px-5 py-3 text-sm font-normal text-left text-gray-800 bg-white border-b border-gray-200"
                   >
-                    Receipt Number
+                    Order Name
                   </th>
                   <th
                     scope="col"
                     className="px-5 py-3 text-sm font-normal text-left text-gray-800 bg-white border-b border-gray-200"
                   >
-                    Member
+                    Amount
                   </th>
                   <th
                     scope="col"
                     className="px-5 py-3 text-sm font-normal text-left text-gray-800  bg-white border-b border-gray-200"
                   >
-                    Order At
+                    Price
                   </th>
                   <th
                     scope="col"
                     className="px-5 py-3 text-sm font-normal text-left text-gray-800  bg-white border-b border-gray-200"
                   >
-                    Receipt At
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-5 py-3 text-sm font-normal text-left text-gray-800 bg-white border-b border-gray-200"
-                  >
-                    Actions
+                    Role
                   </th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <div className="flex items-center">
-                      <div className="flex-shrink-0">
-                        <a href="#" className="relative block">
-                          <img
-                            alt="profil"
-                            src="/images/person/8.jpg"
-                            className="mx-auto object-cover rounded-full h-10 w-10 "
-                          />
-                        </a>
-                      </div>
-                      <div className="ml-3">
-                        <p className="text-gray-900 whitespace-no-wrap">
-                          Jean marc
-                        </p>
-                      </div>
-                    </div>
-                  </td>
-                  <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p className="text-gray-900 whitespace-no-wrap">Admin</p>
-                  </td>
-                  <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <p className="text-gray-900 whitespace-no-wrap">
-                      12/09/2020
-                    </p>
-                  </td>
-                  <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <span className="relative inline-block px-3 py-1 font-semibold leading-tight text-green-900">
-                      <span
-                        aria-hidden="true"
-                        className="absolute inset-0 bg-green-200 rounded-full opacity-50"
-                      ></span>
-                      <span className="relative">active</span>
-                    </span>
-                  </td>
-                  <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                    <a
-                      href="#"
-                      className="text-indigo-600 hover:text-indigo-900"
-                    >
-                      Edit
-                    </a>
-                  </td>
-                </tr>
+                {filteredData?.map((res) => {
+                  return (
+                    <tr key={res.id}>
+                      <td className="py-4 px-6 ">{res.orderNo}</td>
+                      <td className="py-4 px-6 ">{res.product.name}</td>
+                      <td className="py-4 px-6 ">{res.amount}</td>
+                      <td className="py-4 px-6 ">{res.price}</td>
+                      <td className="py-4 px-6 ">{res.user.name}</td>
+                      {/* <td className="py-4 px-6 ">{res.createdAt}</td> */}
+                    </tr>
+                  );
+                })}
               </tbody>
             </table>
-            <div className="flex flex-col items-center px-5 py-5 bg-white xs:flex-row xs:justify-between">
-              <div className="flex items-center">
-                <button
-                  type="button"
-                  className="w-full p-4 text-base text-gray-600 bg-white border rounded-l-xl hover:bg-gray-100"
-                >
-                  <svg
-                    width="9"
-                    fill="currentColor"
-                    height="8"
-                    className=""
-                    viewBox="0 0 1792 1792"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M1427 301l-531 531 531 531q19 19 19 45t-19 45l-166 166q-19 19-45 19t-45-19l-742-742q-19-19-19-45t19-45l742-742q19-19 45-19t45 19l166 166q19 19 19 45t-19 45z"></path>
-                  </svg>
-                </button>
-                <button
-                  type="button"
-                  className="w-full px-4 py-2 text-base text-indigo-500 bg-white border-t border-b hover:bg-gray-100 "
-                >
-                  1
-                </button>
-                <button
-                  type="button"
-                  className="w-full px-4 py-2 text-base text-gray-600 bg-white border hover:bg-gray-100"
-                >
-                  2
-                </button>
-                <button
-                  type="button"
-                  className="w-full px-4 py-2 text-base text-gray-600 bg-white border-t border-b hover:bg-gray-100"
-                >
-                  3
-                </button>
-                <button
-                  type="button"
-                  className="w-full px-4 py-2 text-base text-gray-600 bg-white border hover:bg-gray-100"
-                >
-                  4
-                </button>
-                <button
-                  type="button"
-                  className="w-full p-4 text-base text-gray-600 bg-white border-t border-b border-r rounded-r-xl hover:bg-gray-100"
-                >
-                  <svg
-                    width="9"
-                    fill="currentColor"
-                    height="8"
-                    className=""
-                    viewBox="0 0 1792 1792"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M1363 877l-742 742q-19 19-45 19t-45-19l-166-166q-19-19-19-45t19-45l531-531-531-531q-19-19-19-45t19-45l166-166q19-19 45-19t45 19l742 742q19 19 19 45t-19 45z"></path>
-                  </svg>
-                </button>
-              </div>
-            </div>
           </div>
         </div>
       </div>
