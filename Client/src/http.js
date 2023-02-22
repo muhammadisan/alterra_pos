908f82d20a1cd20e1331fe7613cabd4e8bca153d
muhammadisan <muhammadisan2015@gmail.com>
2023-02-22 22:58:22 +0700

    isanapi

--- Client/src/http.js
+++ Client/src/http.js
@@ -13,6 +13,6 @@ if (user && user.token) {
     }
 }
 export default axios.create({
-    baseURL: "http://34.239.112.166:8080",
+    baseURL: "https://isanapi.capstone-meeting.online",
     headers
 });
